630444a1e8d7137de938af6b220ce427eff2e62e
Harsh <harsh@example.com>
2022-10-14 07:49:17 +0530

    merge fixes

--- pkg/dnsclient/test/testInfoblox.go
+++ pkg/dnsclient/test/testInfoblox.go
@@ -1,8 +1,4 @@
-<<<<<<< HEAD:pkg/dnsclient/testInfoblox.go
-package dnsclient
-=======
 package test
->>>>>>> 46ff0dd49da32ad23ecae5d57a3738254706f5b0:pkg/dnsclient/test/testInfoblox.go
 
 import (
 	"fmt"

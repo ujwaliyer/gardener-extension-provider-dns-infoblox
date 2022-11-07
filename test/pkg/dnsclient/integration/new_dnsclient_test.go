8273792a111bb4ecf8f2d12024da3d45223dc97d
Mayank Raipure <mayank.raipure@sap.com>
2022-11-07 12:26:47 +0530

    add cName , A record and TXT records

--- test/pkg/dnsclient/integration/new_dnsclient_test.go
+++ test/pkg/dnsclient/integration/new_dnsclient_test.go
@@ -22,5 +22,34 @@ var _ = Describe("NewDnsclient", func() {
 			Ω(zones).Should(ContainElement(ContainSubstring("btprpc.supportnix2.com"), &zone))
 			Expect(err).To(BeNil())
 		})
+		It("Should not create A record :", func() {
+			var key string
+			for k := range zone {
+				key = k
+			}
+			var id_addr = []string{"10.16.2.13"}
+			err := dnsClient.CreateOrUpdateRecordSet(nil, "default", key, "example.com", "A", id_addr, 30)
+			Expect(err).NotTo(BeNil())
+		})
+		It("Should create TXT record :", func() {
+			var key string
+			for k := range zone {
+				key = k
+			}
+			var id_addr = []string{"10.16.2.13"}
+			err := dnsClient.CreateOrUpdateRecordSet(nil, "default", key, "txt.example.com", "TXT", id_addr, 30)
+			Expect(err).To(BeNil())
+		})
+
+		It("Should create CNAME record :", func() {
+			var key string
+			for k := range zone {
+				key = k
+			}
+			var id_addr = []string{"10.16.2.13"}
+			//current dns client only create TXT records does not create CName record
+			err := dnsClient.CreateOrUpdateRecordSet(nil, "default", key, "txt.example.com", "CNAME", id_addr, 30)
+			Expect(err).NotTo(BeNil())
+		})
 	})
 })

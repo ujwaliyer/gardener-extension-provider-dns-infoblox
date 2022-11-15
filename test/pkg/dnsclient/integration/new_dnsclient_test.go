8305b4d3d62c670fe833892cdcb805a91d580a97
Mayank Raipure <mayank.raipure@sap.com>
2022-11-15 13:49:43 +0530

    Changed variables in test

--- test/pkg/dnsclient/integration/new_dnsclient_test.go
+++ test/pkg/dnsclient/integration/new_dnsclient_test.go
@@ -11,11 +11,15 @@ import (
 var _ = Describe("NewDnsclient", func() {
 	var dnsClient dnsInfoBlox.DNSClient
 	var zone map[string]string
-	var default_zone = "sujindar.com"
-	var user = "admin"
-	var password = "btprpc_infoblox"
-	var key string
+	const default_zone = "sujindar.com"
+	const user = "admin"
+	const password = "btprpc_infoblox"
+	var value string
+	const a_record_name = "example.com"
+	const txt_record_name = "abcd-efgh"
+	var cname_record_name = "txt" + "." + a_record_name
 	var id_addr = []string{"10.16.2.13"}
+	const dns_view = "default"
 	BeforeEach(func() {
 		Host := "10.16.198.17"
 		dnsC, err := dnsInfoBlox.NewDNSClient(nil, user, password, Host)
@@ -27,36 +31,36 @@ var _ = Describe("NewDnsclient", func() {
 		It("GetManaged zone :", func() {
 			zones, err := dnsClient.GetManagedZones(nil)
 			Ω(zones).Should(ContainElement(ContainSubstring(default_zone), &zone))
-			for k := range zone {
-				key = k
+			for _, v := range zone {
+				value = v
 			}
 			Expect(err).To(BeNil())
 		})
 		It("Should not create A record :", func() {
-			err := dnsClient.CreateOrUpdateRecordSet(nil, "default", key, "example.com", "A", id_addr, 30)
+			err := dnsClient.CreateOrUpdateRecordSet(nil, dns_view, value, a_record_name, "A", id_addr, 30)
 			Expect(err).NotTo(BeNil())
 		})
 		It("Should create TXT record :", func() {
-			err := dnsClient.CreateOrUpdateRecordSet(nil, "default", key, "abcd-efgh"+"."+default_zone, "TXT", id_addr, 30)
+			err := dnsClient.CreateOrUpdateRecordSet(nil, dns_view, value, txt_record_name+"."+value, "TXT", id_addr, 30)
 			Expect(err).To(BeNil())
 		})
 
 		It("Should create CNAME record :", func() {
-			err := dnsClient.CreateOrUpdateRecordSet(nil, "default", key, "txt.example.com", "CNAME", id_addr, 30)
+			err := dnsClient.CreateOrUpdateRecordSet(nil, dns_view, value, cname_record_name, "CNAME", id_addr, 30)
 			Expect(err).NotTo(BeNil())
 		})
 
 		It("Should delete TXT record :", func() {
-			err := dnsClient.DeleteRecordSet(nil, key, "txt"+"."+"infobloxbtprpc", "TXT")
+			err := dnsClient.DeleteRecordSet(nil, value, txt_record_name+"."+value, "TXT")
 			Expect(err).To(BeNil())
 		})
 		It("Should delete A record :", func() {
-			err := dnsClient.DeleteRecordSet(nil, key, "example"+"."+"infobloxbtprpc", "A")
-			Expect(err).To(BeNil())
+			err := dnsClient.DeleteRecordSet(nil, value, a_record_name, "A")
+			Expect(err).NotTo(BeNil())
 		})
 		It("Should delete CNAME record :", func() {
-			err := dnsClient.DeleteRecordSet(nil, key, "def_cname"+"."+"infobloxbtprpc", "CNAME")
-			Expect(err).To(BeNil())
+			err := dnsClient.DeleteRecordSet(nil, value, cname_record_name, "CNAME")
+			Expect(err).NotTo(BeNil())
 		})
 	})
 })

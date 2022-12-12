394a9e0c9fd0a0641dafc9a9a87c067b73da3f7b
mayankraipure11 <56959636+mayankraipure11@users.noreply.github.com>
2022-12-12 15:47:07 +0530

    Test (#51)
    * Segrigation of Testcases and added test def
    
    * Test cases values
    
    * dns lookup tests
    
    * changes to env config
    
    Co-authored-by: Mayank Raipure <mayank.raipure@sap.com>

--- test/pkg/dnsclient/functional/dns_lookup_test.go
+++ test/pkg/dnsclient/functional/dns_lookup_test.go
@@ -1,10 +1,25 @@
-package functional_test
+package functional
 
 import (
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+	"net"
 )
 
 var _ = Describe("DnsLookup", func() {
+	Context("DNS Lookups----", func() {
 
+		It("Rverse Lookup :", func() {
+			namesAssociated, err := net.LookupAddr("10.16.198.140")
+			Expect(namesAssociated).NotTo(BeNil())
+			Expect(err).To(BeNil())
+		})
+
+		It("TXT Lookup :", func() {
+			txtRecord, err := net.LookupTXT("owner.vsphere.garden.internal")
+			Expect(txtRecord).NotTo(BeNil())
+			Expect(err).To(BeNil())
+
+		})
+	})
 })
